a25f113139702613641293715b345aa30711fdfd
dev0715 <toptal126@outlook.com>
2022-05-03 07:33:12 -0700

    fix htmlFor error

--- api/api-client.js
+++ api/api-client.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const createApiClient = () => {
-  const baseURL = 'https://api.railflow.io';
-  //baseURL = 'http://localhost:9000';
+  // const baseURL = 'https://api.railflow.io';
+  const baseURL = 'http://localhost:9000';
   const apiClient = axios.create({
     baseURL: baseURL + '/api',
     headers: {
       'Content-Type': 'application/json;charset=UTF-8',
       'allowed-party-secret': 'shhhsecret',
-      token: process.env.GATSBY_ALLOWED_PARTY_SECRET,
+      token: process.env.ALLOWED_PARTY_SECRET,
     },
   });
 
