32fcba92170984916487af1bc87f5c3babe2b9bd
dev0715 <toptal126@outlook.com>
2022-05-05 08:39:55 -0700

    update images for features section

--- components/feature-sections/feature-list/feature-list.jsx
+++ components/feature-sections/feature-list/feature-list.jsx
@@ -1,42 +1,27 @@
-import React from 'react';
-import classnames from 'classnames/bind';
+import React from "react";
+import classnames from "classnames/bind";
+import Image from "next/image";
 
-import { LayoutSectionContainer } from '../../Layout';
-import * as styles from './feature-list.module.scss';
+import { LayoutSectionContainer } from "../../Layout";
+import * as styles from "./feature-list.module.scss";
 
 const cx = classnames.bind(styles);
 
 const FeatureList = ({ feature }) => {
   const { featureList } = feature;
   return (
-    <section id="feature-list" className={cx('featureList')}>
+    <section id="feature-list" className={cx("featureList")}>
       <LayoutSectionContainer>
         {featureList.map((feature) => (
-          <div
-            key={feature.id}
-            className={cx('featureList_sectionContainer')}
-          >
-            <div
-              className={cx('featureListLeft', 'featureList_section')}
-            >
-              <h1 className={cx('featureListLeft_title')}>
-                {feature.title}
-              </h1>
-              <p className={cx('featureListLeft_text')}>
-                {feature.text}
-              </p>
+          <div key={feature.id} className={cx("featureList_sectionContainer")}>
+            <div className={cx("featureListLeft", "featureList_section")}>
+              <h1 className={cx("featureListLeft_title")}>{feature.title}</h1>
+              <p className={cx("featureListLeft_text")}>{feature.text}</p>
             </div>
-            <div
-              className={cx(
-                'featureListRight',
-                'featureList_section',
-              )}
-            >
-              <img
-                className={cx('featureListRight_image')}
-                src={feature.image}
-                alt="features"
-              />
+            <div className={cx("featureListRight", "featureList_section")}>
+              <div className={cx("featureListRight_image")}>
+                <Image src={feature.image} alt="features" />
+              </div>
             </div>
           </div>
         ))}
--- pages/features/testrail-cucumber.jsx
+++ pages/features/testrail-cucumber.jsx
@@ -1,13 +1,16 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../../components/Layout"
+import Layout from "../../components/Layout";
 // import ComingSoon from '../components/coming-soon';
-import FeaturePage from "../../components/feature-sections"
+import FeaturePage from "../../components/feature-sections";
+import features1Image from "../../public/images/features_1.png";
+import features2Image from "../../public/images/features_2.png";
 
 const CucumberPage = () => {
   const feature = {
-    heroTitle: 'Cucumber TestRail Integration',
-    heroText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas adipisci, corporis recusandae odit dolorem esse cumque porro dolorum tenetur, quisquam voluptatum neque ipsam. Maiores, tempora vero. Perferendis quam adipisci odio. ',
+    heroTitle: "Cucumber TestRail Integration",
+    heroText:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas adipisci, corporis recusandae odit dolorem esse cumque porro dolorum tenetur, quisquam voluptatum neque ipsam. Maiores, tempora vero. Perferendis quam adipisci odio. ",
     featureList: [
       {
         id: "cucumber-feature-1",
@@ -18,7 +21,7 @@ const CucumberPage = () => {
           Nam sem lacus, ornare non ante sed, ultricies fringilla massa.
           Ut congue, elit non tempus elementum, sem risus tincidunt diam, vitae sodales diam ipsum vitae purus.
         `,
-        image: "/images/features_1.png",
+        image: features1Image,
       },
       {
         id: "cucumber-feature-2",
@@ -29,16 +32,15 @@ const CucumberPage = () => {
           Nam sem lacus, ornare non ante sed, ultricies fringilla massa.
           Ut congue, elit non tempus elementum, sem risus tincidunt diam, vitae sodales diam ipsum vitae purus.
         `,
-        image: "/images/features_2.png",
-      }
-    ]
-
-  }
+        image: features2Image,
+      },
+    ],
+  };
   return (
     <Layout>
       <FeaturePage feature={feature} />
     </Layout>
-  )
-}
+  );
+};
 
-export default CucumberPage
+export default CucumberPage;
