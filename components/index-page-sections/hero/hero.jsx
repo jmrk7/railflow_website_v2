91e9aba43bfcff68193c8d68bb28c5120f0c686d
dev0715 <toptal126@outlook.com>
2022-06-09 13:40:10 -0700

    fix index page section hero

--- components/index-page-sections/hero/hero.jsx
+++ components/index-page-sections/hero/hero.jsx
@@ -1,6 +1,5 @@
 import React, { useCallback } from "react";
 import classnames from "classnames/bind";
-import Image from "next/image";
 
 import { LayoutSectionContainer } from "../../Layout";
 import Button from "../../button";
@@ -11,39 +10,41 @@ const cx = classnames.bind(styles);
 
 const Hero = () => {
   const handleClickFeaturesButton = useCallback(() => {
-    const featuresSectionElement = document.getElementById("features");
+    const featuresSectionElement = document.getElementById(
+      'features',
+    );
 
     if (featuresSectionElement) {
       featuresSectionElement.scrollIntoView({
-        block: "start",
-        behavior: "smooth",
+        block: 'start',
+        behavior: 'smooth',
       });
     }
   }, []);
 
   return (
-    <section id="hero" className={cx("hero")}>
+    <section id="hero" className={cx('hero')}>
       <LayoutSectionContainer>
-        <div className={cx("hero_sectionContainer")}>
-          <div className={cx("heroLeft", "hero_section")}>
-            <h1 className={cx("heroLeft_title")}>
-              TestRail Integration Platform and Tools
+        <div className={cx('hero_sectionContainer')}>
+          <div className={cx('heroLeft', 'hero_section')}>
+            <h1 className={cx('heroLeft_title')}>
+              TestRail Integration Platform and Tools 
             </h1>
-            <div className={cx("heroLeft_text")}>
+            <div className={cx('heroLeft_text')}>
               <ul>
-                <li>Integrate TestRail with any CICD platform</li>
-                <li>Centralized test reporting using TestRail</li>
-                <li>Support for countless testing tools and frameworks</li>
-                <li>Zero coding. Up and running in 5 minutes</li>
-                <li>Super charge quality engineering productivity</li>
-                <li>Try Railflow free for 14 days. Satisfaction guaranteed</li>
+              <li>Integrate TestRail with any CICD platform</li>
+              <li>Centralized test reporting using TestRail</li>
+              <li>Support for countless testing tools and frameworks</li>    
+              <li>Zero coding. Up and running in 5 minutes</li>
+              <li>Super charge quality engineering productivity</li>
+              <li>Try Railflow free for 14 days. Satisfaction guaranteed</li>
               </ul>
             </div>
 
-            <div className={cx("heroLeft_buttonContainer")}>
+            <div className={cx('heroLeft_buttonContainer')}>
               <Button
                 onClick={handleClickFeaturesButton}
-                className={cx("heroLeft_button")}
+                className={cx('heroLeft_button')}
               >
                 Explore Features
               </Button>
@@ -51,15 +52,12 @@ const Hero = () => {
             </div>
           </div>
 
-          <div className={cx("heroRight", "hero_section")}>
-            <div className={cx("heroRight_image")}>
-              <Image
-                src={"/images/main_testrail.webp"}
-                alt="testrail"
-                width={610}
-                height={470}
-              />
-            </div>
+          <div className={cx('heroRight', 'hero_section')}>
+            <img
+              className={cx('heroRight_image')}
+              src={"/images/main_testrail.webp"}
+              alt="testrail"
+            />
           </div>
         </div>
       </LayoutSectionContainer>
