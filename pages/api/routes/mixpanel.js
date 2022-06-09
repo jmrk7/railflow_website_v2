4b0ecbfe54dd0648e140ab25451c621f32f01389
dev0715 <lavelle.warren.1995@gmail.com>
2022-06-09 12:11:01 -0700

    add download event to mixpanel

---
+++ pages/api/routes/mixpanel.js
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { sendDataToMixpanel } from "../services/mixpanel";
+
+import Cors from "cors";
+
+const cors = Cors({
+  origin: process.env.ALLOWED_DOMAINS,
+  credentials: true,
+  optionsSuccessStatus: 200,
+  allowedHeaders: "*",
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res, next) {
+  await runMiddleware(req, res, cors);
+
+  if (req.method === "POST") {
+    const dataObject = {
+      "id": req.body.id,
+      "Plugin Name": req.body.name
+    }
+    sendDataToMixpanel(`Download Event`, dataObject)
+    res.send("Success");
+  }
+}
