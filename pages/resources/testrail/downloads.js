bf361522443ef4d8fa5aa39552e409e5f33b365f
dev0715 <lavelle.warren.1995@gmail.com>
2022-06-09 17:47:00 -0700

    change menu

--- pages/resources/testrail/downloads.js
+++ pages/resources/testrail/downloads.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Layout from '../../components/Layout';
+import Layout from '/components/Layout';
 
-import DownloadPosts from "../../components/downloads-sections/download-posts";
+import DownloadPosts from "/components/downloads-sections/download-posts";
 
 const DownloadsPage = () => {
   return (
