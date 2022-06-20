fd6192cafa4ca41660fbe3c66715d50db6a93344
dev0715 <lavelle.warren.1995@gmail.com>
2022-06-20 11:49:15 -0700

    set f4 to true when calling cryptolens api

--- pages/api/services/license.js
+++ pages/api/services/license.js
@@ -48,6 +48,42 @@ async function getCryptolensToken( body, periods = process.env.EXTENSION_PERIOD 
   }
 }
 
+async function getCryptolensTokenByCli( body, periods = process.env.EXTENSION_PERIOD ) {
+  try {
+    const apiClient = await getApiClient(process.env.CRYPTOLENS_BASE_URL);
+    const response = await apiClient.request({
+      method: "POST",
+      url: "/api/key/CreateKey",
+      headers: {
+        // TODO: use environment variable
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: qs.stringify({
+        token: process.env.CRYPTOLENS_API_KEY,
+        ProductId: process.env.PRODUCT_ID,
+        Period: periods,
+        F1: true,
+        F2: true,
+        F4: true,
+        NewCustomer: true,
+        Name: `${body.contact_first_name} ${body.contact_last_name}`,
+        Email: body.contact_email,
+        CompanyName: body.contact_cf_company,
+      }),
+    });
+
+    logger.info(
+      `generated the cryptolens token: ${JSON.stringify(response.data)} for: ${
+        body.contact_email
+      } duration: ${periods} days`
+    );
+    return response.data;
+  } catch (error) {
+    logger.error(`> error generating token: ${error}`);
+    throw new ApiError(`Error while getting cryptolens token`);
+  }
+}
+
 /**
  * Service: Extend a license
  * @param {*} data Input data including key and extension period
@@ -93,4 +129,5 @@ async function extend(data) {
 module.exports = {
   extend,
   getCryptolensToken,
+  getCryptolensTokenByCli,
 };
