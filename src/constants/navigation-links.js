79d6aba09a9ce1c7fb180378f8ba1df175ea1724
dev0715 <lavelle.warren.1995@gmail.com>
2022-06-20 06:49:25 -0700

    remove railflow for zephyr for navbar

--- src/constants/navigation-links.js
+++ src/constants/navigation-links.js
@@ -30,25 +30,25 @@ const navigationLinks = [
           }
         ],
       },
-      {
-        id: 'resources_downloads_zephyr',
-        label: 'Railflow For Zephyr',
-        hasSubmenu: true,
-        to: "/",
-        items: [
-          {
-            id: 'resources_downloads_testrail_download',
-            label: 'Downloads',            
-            to: 'resources/zephyr/downloads'
-          },
-          {
-            id: 'resources_downloads_testrail_doc',
-            label: 'Documentation',            
-            to: 'https://docs.railflow.io',
-            isExternal: true,
-          }
-        ],
-      },
+      // {
+      //   id: 'resources_downloads_zephyr',
+      //   label: 'Railflow For Zephyr',
+      //   hasSubmenu: true,
+      //   to: "/",
+      //   items: [
+      //     {
+      //       id: 'resources_downloads_testrail_download',
+      //       label: 'Downloads',            
+      //       to: 'resources/zephyr/downloads'
+      //     },
+      //     {
+      //       id: 'resources_downloads_testrail_doc',
+      //       label: 'Documentation',            
+      //       to: 'https://docs.railflow.io',
+      //       isExternal: true,
+      //     }
+      //   ],
+      // },
       {
         id: 'resources_support',
         label: 'Customer Support',
