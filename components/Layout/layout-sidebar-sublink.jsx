fe1b7818584cd7d52952f9188021a517de697baf
dev0715 <lavelle.warren.1995@gmail.com>
2022-06-21 06:24:08 -0700

    fix mobile sidebar link

--- components/Layout/layout-sidebar-sublink.jsx
+++ components/Layout/layout-sidebar-sublink.jsx
@@ -3,27 +3,21 @@ import classnames from "classnames/bind";
 import Link from "next/link";
 
 import onEnterKeyDown from "../../utils/on-enter-key-down";
-import SideBarSubLink from "./layout-sidebar-sublink";
+
 import * as styles from "./layout.module.scss";
 
 const cx = classnames.bind(styles);
 
 const LayoutSidebarLink = (props) => {
-  const { label, to, isExternal, dropdownLinks, closeSidebar, onClick, items } =
-    props;
+  const { label, to, isExternal, dropdownLinks, closeSidebar, onClick, } = props;
 
   const hasDropdown = useMemo(
     () => !!(dropdownLinks && dropdownLinks.length),
     [dropdownLinks]
   );
-  
-  const [dropDownShow, setDropDownShow] = useState(false);
+
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const handleDropDown = (e) => {
-    e.preventDefault()
-    setDropDownShow(!dropDownShow)
-  }
   const handleClick = useCallback(() => {
     // INFO: invoke `onClick` funciton received from props.
     if (typeof onClick === "function") {
@@ -44,6 +38,7 @@ const LayoutSidebarLink = (props) => {
 
   return (
     <div
+      onClick={handleClick}
       onKeyDown={onEnterKeyDown(handleClick)}
       role="button"
       tabIndex="-1"
@@ -65,7 +60,7 @@ const LayoutSidebarLink = (props) => {
           </Link>
         )
       ) : (
-        <span className={cx("layoutSidebarLink_label")} onClick={handleClick}>
+        <span className={cx("layoutSidebarLink_label")}>
           {label}
           {hasDropdown && (
             <img
@@ -84,40 +79,13 @@ const LayoutSidebarLink = (props) => {
             layoutSidebarLinkDropdown__close: !isDropdownOpen,
           })}
         >
-          {dropdownLinks.map((dropdownLink) =>
-            dropdownLink.items ? (
-              <>
-              <div key={dropdownLink.id} style={{display: "flex", alignItems: "center"}} onClick={handleDropDown}>
-                <div className={cx("layoutSidebarLinkDropdown_link")} style={{paddingRight: "0px",}}>
-                  {dropdownLink.label}
-                </div>
-                <img
-                  src={"/icons/sort.svg"}
-                  alt="sort"
-                  className={cx("layoutHeaderLink_icon")}
-                  height={8.414}
-                />
+          {dropdownLinks.map((dropdownLink) => (
+            <Link key={dropdownLink.id} href={dropdownLink.to} passHref>
+              <div className={cx("layoutSidebarLinkDropdown_link")}>
+                {dropdownLink.label}
               </div>
-              <div className={cx("layoutSidebarLink")} style={{margin: "0px", padding: "4px 0px 4px 24px", marginLeft: "24px", display: dropDownShow?"block":"none"}}>
-                {dropdownLink.items.map((item) => {
-                  return item.isExternal 
-                  ? (<a key={item.id} href={item.to} rel="noopener noreferrer" target="_blank" className={cx("layoutSidebarLink_label")}>
-                      {item.label}
-                    </a>)
-                  : (<Link key={item.id} href={item.to} passHref>
-                      <div className={cx("layoutSidebarLink_label")} style={{paddingBottom:"12px"}}>{item.label}</div>
-                    </Link>)
-                })}
-            </div>
-            </>
-            ) : (
-              <Link key={dropdownLink.id} href={dropdownLink.to} passHref>
-                <div className={cx("layoutSidebarLinkDropdown_link")}>
-                  {dropdownLink.label}
-                </div>
-              </Link>
-            )
-          )}
+            </Link>
+          ))}
         </div>
       )}
     </div>
