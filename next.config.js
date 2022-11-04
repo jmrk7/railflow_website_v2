6f8241c2605d54937d49a40716d407167aaa2e2b
dev0715 <toptal126@outlook.com>
2022-11-04 08:25:36 -0400

    sentry configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+const { withSentryConfig } = require('@sentry/nextjs');
 const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
+  },
+  sentry: {
+    hideSourceMaps: true,
+    autoInstrumentServerFunctions: false
   },
   env: {
     RECAPTCHA_SITE_KEY: "6Le1WecZAAAAAJHHNtF0A1yOx642M8M0Us5HoJn7",
@@ -73,8 +79,28 @@ const nextConfig = {
         destination: 'https://railflow.atlassian.net/servicedesk/customer/portal/1',
         permanent: true,
       },
+      {
+        source: '/privacy',
+        destination: 'https://docs.railflow.io/docs/legal/privacy',
+        permanent: true,
+      },
+      {
+        source: '/terms',
+        destination: 'https://docs.railflow.io/docs/legal/terms',
+        permanent: true,
+      },
+      
     ]
   },
 };
 
-module.exports = nextConfig;
+
+/**
+ * Sentry webpack plugin options.
+ */
+
+const sentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
